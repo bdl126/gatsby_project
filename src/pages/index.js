1043857d720743ca0f63686f4ff67648cfda19d4
Bruno <bdelafontaine92@gmail.com>
2022-04-03 20:24:35 -0400

    Clean up home page link

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2> I'm Bruno, a full-stack developer living in the beautiful montreal</h2>
-      <p>Need a developer?<Link href="/contact"> Contact me</Link></p>
     </Layout>
   )
 }
